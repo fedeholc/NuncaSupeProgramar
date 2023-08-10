64b86275d51d0128c4d6d507a8cf08963693436f
fedeholc <federicoholc@gmail.com>
2023-08-09 21:26:34 -0300

    fix tags tildes

--- app/tags/[tag]/page.js
+++ app/tags/[tag]/page.js
@@ -5,17 +5,19 @@ import Link from "next/link";
 export default async function Tags({ params }) {
   const allPostsData = await getSortedPostsData();
 
-  //set remueve los duplicados
-  /*   const allTags = new Set(allPostsData.map(({tags}) => tags).flat());
-   */
+  //decodeURI resuelve el problema de las tildes en los tags
 
   return (
     <section>
-      <h2 className="apuntes__titulo">Apuntes con tag {params.tag}:</h2>
+      <h2 className="apuntes__titulo">
+        Apuntes con tag {decodeURI(params.tag)}:
+      </h2>
       <ul>
         {allPostsData
           .filter(({ tags }) =>
-            tags.map((e) => e.toLowerCase()).includes(params.tag.toLowerCase())
+            tags
+              .map((e) => e.toLowerCase())
+              .includes(decodeURI(params.tag).toLowerCase())
           )
           .filter(({ draft }) => draft === false)
           .map(({ id, date, title, draft }) => (
